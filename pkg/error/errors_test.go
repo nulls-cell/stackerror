ffd31f5c3405b0e393585257dd2ea563ac3b5b3e
lr <lr>
2023-12-12 00:20:46 +0800

    feat: add stack error

--- pkg/error/errors_test.go
+++ pkg/error/errors_test.go
@@ -2,7 +2,6 @@ package stackerror
 
 import (
 	"fmt"
-	"k8s.io/klog/v2"
 	"testing"
 )
 
@@ -14,5 +13,5 @@ func TestRuntimeError(t *testing.T) {
 	err4 := NewStackError("normal error")
 	err4 = NewStackErrorf("normal error, args: %s", "arg1")
 	fmt.Println(err3.Error(), err3.GetStack())
-	klog.Info(err4.Error(), err4.GetStack())
+	fmt.Println(err4.Error(), err4.GetStack())
 }
